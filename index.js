6961372815935a6eb16effcffe5fa98c647ff73a
1Regina <1reginacheong@gmail.com>
2022-04-12 00:18:35 +0800

    fix alert for myDonation and myRequest

--- index.js
+++ index.js
@@ -68,7 +68,10 @@ app.post('/signup', async (request, response) => {
   // const updRv = await updateMembership(member, id);
   // if (!updRv) return "ok";
   await updateMembership(member, id);
-  response.send('sign success');
+  // response.send('sign success');
+  const data = {};
+  data.message = 'You have signed up successfully and can donate or request uniforms';
+  response.render('null', { data });
 });
 
 app.get('/add_photo', (request, response) => {
@@ -99,7 +102,9 @@ app.post('/add_photo', singleFileUpload, (request, response) => {
 });
 
 app.get('/login', (request, response) => {
-  response.render('loginForm');
+  const data = {};
+  data.isLogin = '';
+  response.render('loginForm', { data });
 });
 
 app.post('/login', (request, response) => {
@@ -268,38 +273,41 @@ app.get('/donate', (request, response) => {
         console.error(err.message);
       });
   } else {
-    response.send('You need to login in');
+    // response.send('You need to login in');
+    data.message = 'You need to be a member to donate uniforms. Please go to SignUp to be a member or Login';
+    response.render('null', { data });
   }
 });
 
 app.post('/donate', async (request, response) => {
-  if (request.cookies.loggedIn === 'true') {
-    const { userEmail, userID, loggedIn } = request.cookies;
-    const { school, type, quantity } = request.body;
-    const sizing = request.body.size;
-    const size = String(sizing).replace(/ /g, '_').toUpperCase();
-    try {
-      const infoQuery = `SELECT school_id FROM schools WHERE school_name = '${school}'`;
-      const schoolid = await pool.query(infoQuery);
-      const schoolID = schoolid.rows[0].school_id;
-      const findUniId = `SELECT id FROM uniforms WHERE type = '${type}'`;
-      console.log('bbbb', findUniId);
-      const uniID = await pool.query(findUniId);
-      const uID = uniID.rows[0].id;
-      const sqls = [];
-      for (let i = 0; i < quantity; i += 1) {
-        const insertQuery = `INSERT INTO inventory (donor_id, school_id, uniform_id, size) VALUES (${userID}, ${schoolID}, ${uID},'${size}')`;
-        console.log(insertQuery);
-        sqls.push(pool.query(insertQuery));
-      }
-      await Promise.all(sqls);
-      response.send('donating');
-    } catch (err) {
-      console.error(err.message); // wont break
+  // if (request.cookies.loggedIn === 'true') {
+  const { userEmail, userID, loggedIn } = request.cookies;
+  const { school, type, quantity } = request.body;
+  const sizing = request.body.size;
+  const size = String(sizing).replace(/ /g, '_').toUpperCase();
+  try {
+    const infoQuery = `SELECT school_id FROM schools WHERE school_name = '${school}'`;
+    const schoolid = await pool.query(infoQuery);
+    const schoolID = schoolid.rows[0].school_id;
+    const findUniId = `SELECT id FROM uniforms WHERE type = '${type}'`;
+    console.log('bbbb', findUniId);
+    const uniID = await pool.query(findUniId);
+    const uID = uniID.rows[0].id;
+    const sqls = [];
+    for (let i = 0; i < quantity; i += 1) {
+      const insertQuery = `INSERT INTO inventory (donor_id, school_id, uniform_id, size) VALUES (${userID}, ${schoolID}, ${uID},'${size}')`;
+      console.log(insertQuery);
+      sqls.push(pool.query(insertQuery));
     }
-  } else {
-    response.send('Only members can donate');
+    await Promise.all(sqls);
+    response.send('donating');
+  } catch (err) {
+    console.error(err.message); // wont break
   }
+  // } else {
+  //   // response.send('Only members can donate');
+
+  // }
 });
 
 app.get('/my_donations', async (request, response) => {
@@ -325,8 +333,10 @@ app.get('/my_donations', async (request, response) => {
     response.render('showMyDonations', { data });
   } else {
     const data = {};
-    data.message = 'please login to see what you have donated';
-    response.render('null', { data });
+    // data.message = 'Please sign in to see what you have donated';
+    // response.render('null', { data });
+    data.isLogin = false;
+    response.render('loginForm', { data });
   }
 });
 
@@ -350,36 +360,41 @@ app.get('/request', async (request, response) => {
       response.send('Cannot connect');
     }
   } else {
-    response.send('You need to login in');
+    // response.send('You need to login in');
+
+    data.message = 'You need to be a member to request uniforms. Please go to SignUp to be a member or Login';
+    response.render('null', { data });
+    // data.isLogin = false;
+    // response.render('loginForm', { data });
   }
 });
 
 app.post('/request', async (request, response) => {
   const data = {};
-  if (request.cookies.loggedIn === 'true') {
-    const { userEmail, userID, loggedIn } = request.cookies;
-    const { school, type, quantity } = request.body;
-    const sizing = request.body.size;
-    const size = String(sizing).replace(/ /g, '_').toUpperCase();
+  // if (request.cookies.loggedIn === 'true') {
+  const { userEmail, userID, loggedIn } = request.cookies;
+  const { school, type, quantity } = request.body;
+  const sizing = request.body.size;
+  const size = String(sizing).replace(/ /g, '_').toUpperCase();
 
-    try {
-      const findReqQtyQuery = `SELECT COUNT (inventory_id) 
+  try {
+    const findReqQtyQuery = `SELECT COUNT (inventory_id) 
                                FROM donation_request
                                WHERE recipient_id = ${userID} `;
-      const findRecipTot = await pool.query(findReqQtyQuery);
-      const recipientTotal = findRecipTot.rows[0].count;
-      // if ((recipientTotal + quantity) >= 20) {
-      //   // alert('you have exceeded 20 pieces of inventory items. Please find a donor with less quantity to request to stay within your quota for the year');
-      //   data.message = 'you have exceeded 20 pieces of inventory items. Please find a donor with less quantity to request to stay within your quota for the year';
-      //   response.render('null', { data });
-      // }
-      const infoQuery = `SELECT school_id FROM schools WHERE school_name = '${school}'`;
-      const schoolid = await pool.query(infoQuery);
-      const schoolID = schoolid.rows[0].school_id;
-      const findUniId = `SELECT id FROM uniforms WHERE type = '${type}'`;
-      const uniID = await pool.query(findUniId);
-      const uID = uniID.rows[0].id;
-      const findPotentialsQuery = `SELECT donor_id, COUNT(status) 
+    const findRecipTot = await pool.query(findReqQtyQuery);
+    const recipientTotal = findRecipTot.rows[0].count;
+    // if ((recipientTotal + quantity) >= 20) {
+    //   // alert('you have exceeded 20 pieces of inventory items. Please find a donor with less quantity to request to stay within your quota for the year');
+    //   data.message = 'you have exceeded 20 pieces of inventory items. Please find a donor with less quantity to request to stay within your quota for the year';
+    //   response.render('null', { data });
+    // }
+    const infoQuery = `SELECT school_id FROM schools WHERE school_name = '${school}'`;
+    const schoolid = await pool.query(infoQuery);
+    const schoolID = schoolid.rows[0].school_id;
+    const findUniId = `SELECT id FROM uniforms WHERE type = '${type}'`;
+    const uniID = await pool.query(findUniId);
+    const uID = uniID.rows[0].id;
+    const findPotentialsQuery = `SELECT donor_id, COUNT(status) 
                                    FROM inventory WHERE status = 'available' 
                                    AND school_id = ${schoolID} 
                                    AND uniform_id = ${uID} 
@@ -387,46 +402,46 @@ app.post('/request', async (request, response) => {
                                    GROUP BY donor_id 
                                    HAVING COUNT(status) = ${quantity} 
                                    ORDER BY donor_id`;
-      const findDonor = await pool.query(findPotentialsQuery);
+    const findDonor = await pool.query(findPotentialsQuery);
 
-      if (findDonor.rows.length === 0) {
-        data.message = 'Check the inventory page for stocks. You might need to adjust your inventory to match those available by donors';
-        // todo: check why alert is not working
-        // alert('you need to adjust your qty');
-        response.render('null', { data });
-      }
-      const donorFound = findDonor.rows[0].donor_id;
-      console.log(donorFound);
+    if (findDonor.rows.length === 0) {
+      data.message = 'Check the inventory page for stocks. You might need to adjust your inventory to match those available by donors';
+      // todo: check why alert is not working
+      // alert('you need to adjust your qty');
+      response.render('null', { data });
+    }
+    const donorFound = findDonor.rows[0].donor_id;
+    console.log(donorFound);
 
-      const sqls = [];
-      const requestIds = [];
-      for (let i = 0; i < quantity; i += 1) {
-        const updateQuery = `UPDATE inventory SET status = 'reserved' 
+    const sqls = [];
+    const requestIds = [];
+    for (let i = 0; i < quantity; i += 1) {
+      const updateQuery = `UPDATE inventory SET status = 'reserved' 
                              WHERE donor_id = ${donorFound}
                              AND school_id = ${schoolID}
                              AND uniform_id = ${uID}
                              AND size = '${size}' RETURNING id`;
-        sqls.push(pool.query(updateQuery));
-      }
+      sqls.push(pool.query(updateQuery));
+    }
 
-      const rvs = await Promise.all(sqls);
+    const rvs = await Promise.all(sqls);
 
-      // rvs.forEach((rv, idx) => {
-      //   console.log(`Result: ${idx}`, rv.rows);
-      // });
-      const insertSQLs = [];
-      const idObjArray = rvs[0].rows;
-      console.log('11111111111', idObjArray);
-      for (let j = 0; j < idObjArray.length; j += 1) {
-        const ind = idObjArray[j].id;
-        requestIds.push(ind);
-        const requestTBQuery = `INSERT INTO donation_request (recipient_id, inventory_id) VALUES (${userID}, ${ind})`;
-        insertSQLs.push(pool.query(requestTBQuery));
-      }
-      console.log(insertSQLs);
-      await Promise.all(insertSQLs);
-      // find and alert donor
-      const findDonorQuery = `SELECT email, name, COUNT(reserved_date), 
+    // rvs.forEach((rv, idx) => {
+    //   console.log(`Result: ${idx}`, rv.rows);
+    // });
+    const insertSQLs = [];
+    const idObjArray = rvs[0].rows;
+    console.log('11111111111', idObjArray);
+    for (let j = 0; j < idObjArray.length; j += 1) {
+      const ind = idObjArray[j].id;
+      requestIds.push(ind);
+      const requestTBQuery = `INSERT INTO donation_request (recipient_id, inventory_id) VALUES (${userID}, ${ind})`;
+      insertSQLs.push(pool.query(requestTBQuery));
+    }
+    console.log(insertSQLs);
+    await Promise.all(insertSQLs);
+    // find and alert donor
+    const findDonorQuery = `SELECT email, name, COUNT(reserved_date), 
                                  school_name, type, size
                           FROM users 
                           INNER JOIN inventory 
@@ -439,48 +454,48 @@ app.post('/request', async (request, response) => {
                           ON uniforms.id = uniform_id
                           WHERE reserved_date::date = now()::date
                           GROUP BY email, name, school_name, type, size`;
-      const resultDonor = await pool.query(findDonorQuery);
-      const num = resultDonor.rows.length;
-      const lastReq = resultDonor.rows[num - 1];
-      console.log(lastReq);
+    const resultDonor = await pool.query(findDonorQuery);
+    const num = resultDonor.rows.length;
+    const lastReq = resultDonor.rows[num - 1];
+    console.log(lastReq);
 
-      response.redirect('/my_requests');
+    response.redirect('/my_requests');
 
-      // const sgMail = require('@sendgrid/mail')();
+    // const sgMail = require('@sendgrid/mail')();
 
-      sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-      const msg = {
-        // to: [{'1reginacheong@gmail.com'},{}], // Change to your recipient
-        to: [
-          {
-            email: '1reginacheong@gmail.com',
-          },
-          {
-            email: `${lastReq.email}`,
-          },
-        ],
-        from: 'regina_cheong@hotmail.com', // Change to your verified sender
-        subject: `There is a request for your donated ${lastReq.school_name} uniforms`,
-        text: `There is a request for the ${lastReq.count} ${lastReq.school_name} ${lastReq.type} of size ${lastReq.size}. lalala `,
-        html: `<strong>There is a request for your ${lastReq.count} piece(s) of ${lastReq.school_name} ${lastReq.type} of size ${lastReq.size}.</strong>`,
-      };
-      sgMail
-        .send(msg)
-        .then(() => {
-          console.log('Email sent');
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-      data.message = 'Request Successful and the donor is notified via email';
-      response.render('null', { data });
-    } catch (err) {
-      console.error(err.message); // wont break
-    }
-  } else {
-    data.message = 'Only members can request';
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const msg = {
+      // to: [{'1reginacheong@gmail.com'},{}], // Change to your recipient
+      to: [
+        {
+          email: '1reginacheong@gmail.com',
+        },
+        {
+          email: `${lastReq.email}`,
+        },
+      ],
+      from: 'regina_cheong@hotmail.com', // Change to your verified sender
+      subject: `There is a request for your donated ${lastReq.school_name} uniforms`,
+      text: `There is a request for the ${lastReq.count} ${lastReq.school_name} ${lastReq.type} of size ${lastReq.size}. lalala `,
+      html: `<strong>There is a request for your ${lastReq.count} piece(s) of ${lastReq.school_name} ${lastReq.type} of size ${lastReq.size}.</strong>`,
+    };
+    sgMail
+      .send(msg)
+      .then(() => {
+        console.log('Email sent');
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    data.message = 'Request Successful and the donor is notified via email';
     response.render('null', { data });
+  } catch (err) {
+    console.error(err.message); // wont break
   }
+  // } else {
+  //   data.message = 'Only members can request';
+  //   response.render('null', { data });
+  // }
 });
 
 app.get('/my_requests', async (request, response) => {
@@ -507,8 +522,10 @@ app.get('/my_requests', async (request, response) => {
     response.render('showMyRequests', { data });
   } else {
     const data = {};
-    data.message = 'please login to see what you have requested';
-    response.render('null', { data });
+    // data.message = 'please login to see what you have requested';
+    // response.render('null', { data });
+    data.isLogin = false;
+    response.render('loginForm', { data });
   }
 });
 
