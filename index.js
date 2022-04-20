b5fa2ac7233e6aac6e1cbb428274a37e8bff42c4
1Regina <1reginacheong@gmail.com>
2022-04-21 02:17:32 +0800

    delete unnecessary ejs

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express, { response } from 'express';
+import express from 'express';
 import methodOverride from 'method-override';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
